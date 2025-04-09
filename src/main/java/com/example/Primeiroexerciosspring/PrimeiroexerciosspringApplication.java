47e00bb7bf867685fa0622a3b8f49b6d3ca89974
NandoJunioDev <frnndmaz_cf@outlook.com>
2025-04-09 20:52:37 -0300

    first commit

---
+++ src/main/java/com/example/Primeiroexerciosspring/PrimeiroexerciosspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Primeiroexerciosspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroexerciosspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroexerciosspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Primeiroexerciosspring/PrimeiroexerciosspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Primeiroexerciosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroexerciosspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
